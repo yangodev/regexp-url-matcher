607da27724dbc6a9722828b452326486f2cbcd69
y4ng0 <y4ng0@yangodev>
2015-02-02 02:56:51 +0100

    initial commit

---
+++ matcher.js
@@ -0,0 +1,134 @@
+var pattern = {
+	RE_EXTRACT : /\/?((\{(\w*)\})|\/?(\[(.*)\])|(\w+))(\s*\|\s*(\S*))?/,
+	init : function(str, config){
+		this.delimiter = config.delimiter || '\\/'
+		this.analyzed = this.analyze(str)
+
+		var delimiter = this.delimiter
+		var analyzed = this.analyzed
+
+		var anchors = []
+		var regex_str = analyzed.map(function(part){
+			if (!part) return null
+			return part.regex
+		})
+
+		if (regex_str.indexOf(null) > -1) return null
+
+		this.definition = str
+		this.regex = new RegExp(regex_str.join(''))
+
+		console.log(this)
+		return this
+	},
+
+	analyze : function(str){
+		var self = this
+		var parts = str.match(new RegExp('[^' + this.delimiter + ']+', 'g'))
+		var idents = []
+
+		return parts.map(function(part){
+			var extracted = self.RE_EXTRACT.exec(part)
+
+			var param = extracted[3]
+			var sub_regex = extracted[5]
+			var name = extracted[6]
+			var modifier = extracted[8]
+
+			var info = {
+				regex : null,
+				anchor : null
+			}
+
+			var begin = ['((?:', self.delimiter, '?(?:'].join('')
+			var end = ['))', modifier, ')'].join('')
+
+			if (name && !name.length) return null
+			else if (param !== undefined){
+				info.anchor = (param.length) ? param : '_'
+				// ((?:{{delimiter}}(?:\w+)){{modifier}})
+				info.regex = [begin, '\\w+', end].join('')
+			}
+
+			else if (sub_regex !== undefined){
+				// ((?:{{delimiter}}(?:{{custom RegExp}})){{modifier}})
+				info.anchor = '@eval'
+				info.regex = [begin, sub_regex, end].join('')
+			}
+			else {
+				info.name = name
+				info.regex = [self.delimiter, '?', name].join('')
+			}
+			if (info.anchor){
+				while (idents.indexOf(info.anchor) > -1) { info.anchor = info.anchor + '_' }
+				idents.push(info.anchor)
+			}
+
+			return info
+		})
+	},
+	match : function(str){
+		var result = this.regex.exec(str)
+
+		if (!result) return null
+		else result = result.slice(1)
+
+		var index  = 0
+		var params = {}
+
+		for (var i in this.analyzed){
+			var ident = this.analyzed[i]
+			if (!ident.anchor) continue;
+
+			var content = result[index].match(/[^\/]+/g)
+			if (content.length === 1) content = content[0]
+
+			params[indent.anchor] = content
+			index++
+		}
+
+		return params
+	},
+	reverse : function(params){
+		params = params || {}
+		var analyzed  = this.analyzed
+		var string = []
+		var added = []
+
+		var missing = []
+		var unsufficient = []
+
+
+		while (analyzed.length){
+			var part = analyzed.shift()
+			var anchor = part.anchor
+			if (!anchor){ string.push(part.name); continue; }
+			else if (!params[anchor]){ missing.push(anchor); continue; }
+
+
+			var joined = [].concat(params[anchor]).join(this.unescape(this.delimiter))
+
+			if (new RegExp(part.regex).test(joined)) {
+				string.push(joined)
+			}
+			else {
+				unsufficient.push(part.anchor)
+			}
+		}
+		if (missing.length || unsufficient.length) return {
+			missing : missing,
+			unsufficient : unsufficient
+		}
+		return string.join(this.unescape(this.delimiter))
+	},
+	unescape : function(str){
+		return str.match(/[^(\\\\)]+/g).join('')
+	}
+
+}
+
+var  api = module.exports = exports = {
+	create : function(str, config){
+		return Object.create(pattern).init(str,config || {})
+	}
+}
---
+++ tests.matcher.js
@@ -0,0 +1,14 @@
+var patt = api.create('/{}|+/stop/{param}|{2}/[\\d+g]|{2}/{}|+/end')
+console.log(patt.match('/b/stop/c/d/3g/6g/gar/bidge/end'))
+console.log(
+	patt.reverse({
+		_: 'b',
+		param: [ 'c', 'd' ],
+		'@eval': [ '3g', '6g' ],
+		__: [ 'gar', 'bidge' ]
+	})
+)
+
+var patt2 = api.create('{}|*.google.{}|*', {delimiter : '\\.'})
+console.log(patt2.match('mail.google.com'))
+console.log(patt2.reverse({_: 'mail', __:'com'}))
